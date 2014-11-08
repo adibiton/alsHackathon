a0a67c6aae655e8a162ad65f025d0ad9f5f6a438
adi.biton <adi.biton@sap.com>
2014-11-09 00:38:54 +0200

    Add new files

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\I041550\Documents\GitHub\alsHackathon\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\I041550\Documents\GitHub\alsHackathon\platforms\android\AndroidManifest.xml \
